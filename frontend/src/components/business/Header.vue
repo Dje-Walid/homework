3ecd2974eac8e47d90cca08f9e5780300fb1adf0
Djemmaa Walid <walid.djemmaa@supdevinci-edu.fr>
2024-03-08 11:19:51 +0100

    init pject

---
+++ frontend/src/components/business/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header class="border-b-2 border-black">
+    <div class="flex justify-between mx-10">
+      <div class="flex flex-row items-center">
+        <img src=""><img>
+      </div>
+      <div class="flex flex-row items-center mr-8 gap-6">
+        <router-link to="/pricing" class="hover:text-blue-400">Pricing</router-link>
+        <router-link to="/about" class="hover:text-blue-400">About us</router-link>
+        <router-link to="/sign" class="hover:text-blue-400">Sign in</router-link>
+      </div>
+    </div>
+  </header>
+</template>
